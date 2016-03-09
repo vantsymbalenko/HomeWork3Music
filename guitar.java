2b67259a7ac806aab755bb22f801781518e2a877
vantsymbalenko <ivanzymbalenko1@gmail.com>
2016-03-09 02:42:30 +0200

    Added files via upload

---
+++ guitar.java
@@ -0,0 +1,7 @@
+package MusicInstruments;
+
+/**
+ * Created by Admin on 09.03.2016.
+ */
+public class guitar extends musicInstrument {
+}
---
+++ shop.java
@@ -0,0 +1,10 @@
+package MusicInstruments;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 09.03.2016.
+ */
+public class shop {
+    private List<musicInstrument> MusicInstruments;
+}
